2eb6dc89f9bb080943944a9c8ed8bcb684613b0e
Thryrallo <thryrallo@gmail.com>
2022-07-26 02:32:54 +0200

    Renamed stuff to VRBrations
    folder, solution etc.

--- VRBrations/Properties/Resources.Designer.cs
+++ VRBrations/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace VRCToyController.Properties {
+namespace VRbrations.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace VRCToyController.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VRCToyController.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VRbrations.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
